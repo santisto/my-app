a5224ae6bf49065df6296fe3698d10c5ad3de93e
Doriss333 <dmlcottini@escuelasproa.edu.ar>
2023-06-06 10:19:29 -0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EventosComponent} from './pages/eventos/eventos.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'eventos', component:EventosComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
