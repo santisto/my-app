abb659219f8001731111bf5892d9358c37bd3718
Abril05F <129186275+Abril05F@users.noreply.github.com>
2023-06-06 10:21:49 +0200

    Eventos y cartas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { EventosComponent } from './pages/eventos/eventos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent, 
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
